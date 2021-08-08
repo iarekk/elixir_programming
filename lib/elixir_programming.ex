ecbdcf8eb3a7d190d0bc8afca8238916218d8e95
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2021-08-08 20:19:21 +0100

    mix init

---
+++ lib/elixir_programming.ex
@@ -0,0 +1,18 @@
+defmodule ElixirProgramming do
+  @moduledoc """
+  Documentation for `ElixirProgramming`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirProgramming.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
