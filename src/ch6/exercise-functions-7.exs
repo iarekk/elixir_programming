613207cc7cda99580f6e6d7714f575c70a31ebb3
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2021-08-08 20:53:22 +0100

    end ch6

---
+++ src/ch6/exercise-functions-7.exs
@@ -0,0 +1,22 @@
+#– Convert a float to a string with two decimal digits. (Erlang)
+
+# :erlang.float_to_binary/2
+# :erlang.float_to_binary(1333.339, [decimals: 2])
+
+:erlang.float_to_binary(1333.339, [decimals: 2])
+
+#– Get the value of an operating-system environment variable. (Elixir)
+
+System.get_env("PATH")
+
+#– Return the extension component of a file name (so return .exs if given "dave/test.exs"). (Elixir)
+
+Path.extname("dave/test.exs")
+
+#– Return the process’s current working directory. (Elixir)
+
+File.cwd
+
+#– Convert a string containing JSON into Elixir data structures. (Just
+#find; don’t install.)
+#– Execute a command in your operating system’s shell.
