ac7bbad12ace91a1e2cd805ac2310752b47e4ace
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2021-08-08 23:32:30 +0100

    ch 7 ex 2

--- src/ch7/mylist.exs
+++ src/ch7/mylist.exs
@@ -15,6 +15,12 @@ defmodule Mylist do
     mapped = map(lst, f)
     reduce(mapped, 0, &(&1 + &2))
   end
+
+  def max([head | tail]), do: _max(head, tail)
+
+  defp _max(m, []), do: m
+  defp _max(m, [h | t]) when h > m, do: _max(h, t)
+  defp _max(m, [_ | t]), do: _max(m, t)
 end
 
 # (1..10) |> Enum.to_list |> Mylist.len
@@ -22,3 +28,5 @@ end
 # [a,b,c] = Mylist.map [4,5,6], fn (n) -> n*3 end
 # Mylist.map [4,5,6], &(&1 * &1)
 # Mylist.reduce [1,2,3], 100, &(&1 * &2)
+# 10
+IO.puts(Mylist.max([5, 4, 1, 2, 10, 3, 4, 7]))
