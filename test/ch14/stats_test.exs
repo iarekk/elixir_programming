82e76109b40a4bc1fd97b8776131c77a8082153b
Iarek Kovtunenko <iarek.kovtunenko@emailage.com>
2022-07-21 18:24:42 +0100

    Introduce the describe

--- test/ch14/stats_test.exs
+++ test/ch14/stats_test.exs
@@ -3,18 +3,20 @@ defmodule Ch14.StatsTest do
 
   doctest Ch14.Stats
 
-  test "calculates sum" do
-    list = [1, 3, 5, 7, 9]
-    assert Ch14.Stats.sum(list) == 25
-  end
+  describe "stats on lists of ints" do
+    test "calculates sum" do
+      list = [1, 3, 5, 7, 9]
+      assert Ch14.Stats.sum(list) == 25
+    end
 
-  test "calculates count" do
-    list = [1, 3, 5, 7, 9]
-    assert Ch14.Stats.count(list) == 5
-  end
+    test "calculates count" do
+      list = [1, 3, 5, 7, 9]
+      assert Ch14.Stats.count(list) == 5
+    end
 
-  test "calculates average" do
-    list = [1, 3, 5, 7, 9]
-    assert Ch14.Stats.average(list) == 5
+    test "calculates average" do
+      list = [1, 3, 5, 7, 9]
+      assert Ch14.Stats.average(list) == 5
+    end
   end
 end
