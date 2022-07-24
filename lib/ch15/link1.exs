719d785eb0d5783d3e80059229974acccc1740ff
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2022-07-24 01:39:25 +0100

    Linkin processes (#6)
    * Unlinked processes
    
    * run in the end
    
    * inspect the message
    
    * linked processes

---
+++ lib/ch15/link1.exs
@@ -0,0 +1,20 @@
+defmodule Ch15.Link1 do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep(500)
+    exit(:boom)
+  end
+
+  def run do
+    spawn(Ch15.Link1, :sad_function, [])
+
+    receive do
+      msg -> IO.puts("Message received: #{inspect(msg)}")
+    after
+      1000 -> "Nothing has happened, ALL IS WELL"
+    end
+  end
+end
+
+Ch15.Link1.run()
