efea41393c99fefd396a00d3737da9caeab506b5
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2022-07-24 01:31:46 +0100

    Unlinked processes

---
+++ lib/ch15/link1.exs
@@ -0,0 +1,18 @@
+defmodule Ch15.Link1 do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep(500)
+    exit(:boom)
+  end
+
+  def run do
+    spawn(Ch15.Link1, :sad_function, [])
+
+    receive do
+      msg -> IO.puts("Message received: #{msg}")
+    after
+      1000 -> "Nothing has happened, ALL IS WELL"
+    end
+  end
+end
