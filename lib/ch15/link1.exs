d8b82878f6ce5294f20a4e2b6873f20ed24a1eb6
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2022-07-24 01:33:10 +0100

    inspect the message

--- lib/ch15/link1.exs
+++ lib/ch15/link1.exs
@@ -10,7 +10,7 @@ defmodule Ch15.Link1 do
     spawn(Ch15.Link1, :sad_function, [])
 
     receive do
-      msg -> IO.puts("Message received: #{msg}")
+      msg -> IO.puts("Message received: #{inspect(msg)}")
     after
       1000 -> "Nothing has happened, ALL IS WELL"
     end
