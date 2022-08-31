e931779ec159e3da7ec733b9ef569ccbc8c858f0
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2022-09-01 00:44:40 +0100

    very basic and wrong implementation of round-robin

--- lib/ch16/exercise3/ticker_client_roundrobin.ex
+++ lib/ch16/exercise3/ticker_client_roundrobin.ex
@@ -1,14 +1,14 @@
 defmodule Ch16.Exercise3.TickerClientRoundrobin do
-  def start do
-    pid = spawn(__MODULE__, :receiver, [])
+  def start(client_number) do
+    pid = spawn(__MODULE__, :receiver, [client_number])
     Ch16.Exercise3.TickerRoundrobin.register(pid)
   end
 
-  def receiver do
+  def receiver(client_number) do
     receive do
       {:tick, num} ->
-        IO.puts("tock ##{num} in client")
-        receiver()
+        IO.puts("tock ##{num} in client ##{client_number}")
+        receiver(client_number)
     end
   end
 end
