a5d45b1fcc6964524a3757ac0c3be5206733c626
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2022-09-04 19:36:08 +0100

    interim progress - race condition on registrations

--- lib/ch16/exercise4/ticker_ring.exs
+++ lib/ch16/exercise4/ticker_ring.exs
@@ -12,10 +12,6 @@ defmodule Ch16.Exercise4.TickerRing do
   # @name :ticker_ring
 
   def generate(client_id, next_pid, is_active) do
-    if(is_nil(next_pid)) do
-      send(self(), {:repoint, self()})
-    end
-
     receive do
       {:tick, sender_id} ->
         IO.puts("client ##{client_id} received tick from ##{sender_id}")
@@ -26,11 +22,20 @@ defmodule Ch16.Exercise4.TickerRing do
           "client ##{client_id} received registration with PID ##{inspect(new_client_pid)}, current next in line: #{inspect(next_pid)}"
         )
 
-        IO.puts(
-          "sending repoint request to #{inspect(new_client_pid)}, repointing to #{inspect(next_pid)}"
-        )
+        if(is_nil(next_pid)) do
+          IO.puts(
+            "sending repoint request to #{inspect(new_client_pid)}, repointing to #{inspect(self())}"
+          )
 
-        send(new_client_pid, {:repoint, next_pid})
+          send(new_client_pid, {:repoint, self()})
+        else
+          IO.puts(
+            "sending repoint request to #{inspect(new_client_pid)}, repointing to #{inspect(next_pid)}"
+          )
+
+          send(new_client_pid, {:repoint, next_pid})
+        end
+
         generate(client_id, new_client_pid, is_active)
 
       {:repoint, new_client_pid} ->
@@ -44,7 +49,7 @@ defmodule Ch16.Exercise4.TickerRing do
         if(is_active) do
           IO.puts("tick at ##{client_id}, sending to #{inspect(next_pid)}")
 
-          if(next_pid != self()) do
+          if(next_pid != nil) do
             send(next_pid, {:tick, client_id})
             generate(client_id, next_pid, false)
           else
@@ -59,13 +64,19 @@ defmodule Ch16.Exercise4.TickerRing do
 end
 
 pid1 = spawn(Ch16.Exercise4.TickerRing, :generate, ["1", nil, true])
-:timer.sleep(10)
+IO.puts("pid1: #{inspect(pid1)}")
 pid2 = spawn(Ch16.Exercise4.TickerRing, :generate, ["2", nil, false])
-:timer.sleep(10)
+IO.puts("pid2: #{inspect(pid2)}")
 send(pid1, {:register, pid2})
 pid3 = spawn(Ch16.Exercise4.TickerRing, :generate, ["3", nil, false])
-:timer.sleep(10)
+IO.puts("pid3: #{inspect(pid3)}")
 send(pid2, {:register, pid3})
 pid4 = spawn(Ch16.Exercise4.TickerRing, :generate, ["4", nil, false])
-:timer.sleep(10)
+IO.puts("pid4: #{inspect(pid4)}")
 send(pid3, {:register, pid4})
+
+# A -> B -> C -> A
+# global handle pointing at A
+# D -> A, then C must point to D (D -> A -> B -> C -> D)
+# needed: update C, furthermost from A
+# D -> B, then A must point to D
