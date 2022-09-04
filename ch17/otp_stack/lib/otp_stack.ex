cf7a654a9f460281e13051df6427b965817e25a1
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2022-09-04 23:39:29 +0100

    OTP Stack server: implement the `pop` interface (#18)

---
+++ ch17/otp_stack/lib/otp_stack.ex
@@ -0,0 +1,18 @@
+defmodule OtpStack do
+  @moduledoc """
+  Documentation for `OtpStack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OtpStack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
